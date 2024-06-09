ba5f4fa2d2e38e83f0f9e0482e73d9ea825330a7
zmzlois <lois.z@comcord.vision>
2024-06-09 17:41:05 +0100

    modified rspress config

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  base: "/reading-rspack/",
+  base: "/",
   title: 'Reading Rspack',
   description: 'Reading Rspack source code',
   icon: '/rspress-icon.png',
